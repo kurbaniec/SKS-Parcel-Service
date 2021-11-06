835ec6121a7bbb0113e81f4add7862e436851cab
David Zelenay <david.zelenay@tietoevry.com>
2021-11-06 14:01:09 +0100

    Mocked repos for BL tests

--- UrbaniecZelenay.SKS.Package.BusinessLogic.Tests/RecipientLogicTest.cs
+++ UrbaniecZelenay.SKS.Package.BusinessLogic.Tests/RecipientLogicTest.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using AutoMapper;
 using FluentValidation;
+using Moq;
 using NUnit.Framework;
 using UrbaniecZelenay.SKS.Package.BusinessLogic.Entities;
 using UrbaniecZelenay.SKS.Package.BusinessLogic.Interfaces;
+using UrbaniecZelenay.SKS.Package.BusinessLogic.Mappings;
 using UrbaniecZelenay.SKS.Package.BusinessLogic.Validators;
+using UrbaniecZelenay.SKS.Package.DataAccess.Interfaces;
+using DalParcel = UrbaniecZelenay.SKS.Package.DataAccess.Entities.Parcel;
+using DalRecipient = UrbaniecZelenay.SKS.Package.DataAccess.Entities.Recipient;
+using DalHopArrival = UrbaniecZelenay.SKS.Package.DataAccess.Entities.HopArrival;
 
 namespace UrbaniecZelenay.SKS.Package.BusinessLogic.Tests
 {
@@ -21,7 +28,37 @@ namespace UrbaniecZelenay.SKS.Package.BusinessLogic.Tests
         public void TrackParcel_ValidTrackingId_ParcelReturned()
         {
             string trackingId = "PYJRB4HZ6";            
-            IRecipientLogic recipientLogic = new RecipientLogic();
+            Mock<IParcelRepository> mockParcelRepo = new Mock<IParcelRepository>();
+            mockParcelRepo.Setup(m => m.GetByTrackingId(It.IsAny<string>())).Returns(new DalParcel
+            {
+                TrackingId = "PYJRB4HZ6",
+                Weight = 1,
+                Recipient = new DalRecipient
+                {
+                    Name = "Max Mustermann",
+                    Street = "A Street",
+                    PostalCode = "1200",
+                    City = "Vienna",
+                    Country = "Austria"
+                },
+                Sender = new DalRecipient
+                {
+                    Name = "Max Mustermann",
+                    Street = "A Street",
+                    PostalCode = "1200",
+                    City = "Vienna",
+                    Country = "Austria"
+                },
+                State = DalParcel.StateEnum.TransferredEnum,
+                VisitedHops = new List<DalHopArrival>(),
+                FutureHops = new List<DalHopArrival>()
+            });
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingsProfileBlDal());
+            });
+            
+            IRecipientLogic recipientLogic = new RecipientLogic(mockParcelRepo.Object, mapperConfig.CreateMapper());
             Parcel blParcel = recipientLogic.TrackParcel(trackingId);
             IValidator<Parcel> parcelValidator = new ParcelValidator();
             var validationResult = parcelValidator.Validate(blParcel);
@@ -31,7 +68,37 @@ namespace UrbaniecZelenay.SKS.Package.BusinessLogic.Tests
         public void TrackParcel_TrackingIdNull_ExceptionThrown()
         {
             string trackingId = null;            
-            IRecipientLogic recipientLogic = new RecipientLogic();
+            Mock<IParcelRepository> mockParcelRepo = new Mock<IParcelRepository>();
+            mockParcelRepo.Setup(m => m.GetByTrackingId(It.IsAny<string>())).Returns(new DalParcel
+            {
+                TrackingId = "PYJRB4HZ6",
+                Weight = 1,
+                Recipient = new DalRecipient
+                {
+                    Name = "Max Mustermann",
+                    Street = "A Street",
+                    PostalCode = "1200",
+                    City = "Vienna",
+                    Country = "Austria"
+                },
+                Sender = new DalRecipient
+                {
+                    Name = "Max Mustermann",
+                    Street = "A Street",
+                    PostalCode = "1200",
+                    City = "Vienna",
+                    Country = "Austria"
+                },
+                State = DalParcel.StateEnum.TransferredEnum,
+                VisitedHops = new List<DalHopArrival>(),
+                FutureHops = new List<DalHopArrival>()
+            });
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingsProfileBlDal());
+            });
+            
+            IRecipientLogic recipientLogic = new RecipientLogic(mockParcelRepo.Object, mapperConfig.CreateMapper());
             Assert.Throws<ArgumentNullException>(() => recipientLogic.TrackParcel(trackingId));
         }
     }
--- UrbaniecZelenay.SKS.Package.BusinessLogic.Tests/WarehouseManagementLogicTest.cs
+++ UrbaniecZelenay.SKS.Package.BusinessLogic.Tests/WarehouseManagementLogicTest.cs
@@ -1,9 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using AutoMapper;
+using Moq;
 using NUnit.Framework;
 using UrbaniecZelenay.SKS.Package.BusinessLogic.Entities;
 using UrbaniecZelenay.SKS.Package.BusinessLogic.Interfaces;
+using UrbaniecZelenay.SKS.Package.BusinessLogic.Mappings;
+using UrbaniecZelenay.SKS.Package.DataAccess.Interfaces;
+using DalParcel = UrbaniecZelenay.SKS.Package.DataAccess.Entities.Parcel;
+using DalRecipient = UrbaniecZelenay.SKS.Package.DataAccess.Entities.Recipient;
+using DalHopArrival = UrbaniecZelenay.SKS.Package.DataAccess.Entities.HopArrival;
+using DalHop = UrbaniecZelenay.SKS.Package.DataAccess.Entities.Hop;
 
 namespace UrbaniecZelenay.SKS.Package.BusinessLogic.Tests
 {
@@ -18,16 +26,48 @@ namespace UrbaniecZelenay.SKS.Package.BusinessLogic.Tests
         [Test]
         public void ExportWarehouse_InternalError_ExceptionThrown()
         {
+            Mock<IWarehouseRepository> mockWarehouseRepo = new Mock<IWarehouseRepository>();
+            mockWarehouseRepo.Setup(m => m.GetAll()).Returns(new DataAccess.Entities.Warehouse
+            {
+                Code = "AUTA01",
+                Description = "Root Warehouse - Österreich",
+                HopType = "Warehouse",
+                LocationCoordinates = new DataAccess.Entities.GeoCoordinate { Lat = 47.247829, Lon = 13.884382 },
+                LocationName = "Root",
+                ProcessingDelayMins = 186,
+                Level = 0,
+                NextHops = new List<DataAccess.Entities.WarehouseNextHops>()
+            });
+
+            var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingsProfileBlDal()); });
+
             IWarehouseManagementLogic warehouseManagementLogic =
-                new WarehouseManagementLogic { TriggerExportWarehouseException = true };
+                new WarehouseManagementLogic(mockWarehouseRepo.Object, mapperConfig.CreateMapper());
+            warehouseManagementLogic.TriggerExportWarehouseException = true;
             Assert.Throws<InvalidOperationException>(() => warehouseManagementLogic.ExportWarehouses());
         }
 
         [Test]
         public void ExportWarehouse_Valid_AllWarehousesReturned()
         {
+            Mock<IWarehouseRepository> mockWarehouseRepo = new Mock<IWarehouseRepository>();
+            mockWarehouseRepo.Setup(m => m.GetAll()).Returns(new DataAccess.Entities.Warehouse
+            {
+                Code = "AUTA01",
+                Description = "Root Warehouse - Österreich",
+                HopType = "Warehouse",
+                LocationCoordinates = new DataAccess.Entities.GeoCoordinate { Lat = 47.247829, Lon = 13.884382 },
+                LocationName = "Root",
+                ProcessingDelayMins = 186,
+                Level = 0,
+                NextHops = new List<DataAccess.Entities.WarehouseNextHops>()
+            });
+
+
+            var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingsProfileBlDal()); });
+
             IWarehouseManagementLogic warehouseManagementLogic =
-                new WarehouseManagementLogic { TriggerExportWarehouseException = false };
+                new WarehouseManagementLogic(mockWarehouseRepo.Object, mapperConfig.CreateMapper());
             Warehouse result = warehouseManagementLogic.ExportWarehouses();
             Assert.NotNull(result);
         }
@@ -36,7 +76,24 @@ namespace UrbaniecZelenay.SKS.Package.BusinessLogic.Tests
         public void GetWarehouse_ValidCode_WarehouseReturned()
         {
             string code = "AUTA05";
-            IWarehouseManagementLogic warehouseManagementLogic = new WarehouseManagementLogic();
+            Mock<IWarehouseRepository> mockWarehouseRepo = new Mock<IWarehouseRepository>();
+            mockWarehouseRepo.Setup(m => m.GetWarehouseByCode(It.IsAny<string>())).Returns(
+                new DataAccess.Entities.Warehouse
+                {
+                    Code = "AUTA01",
+                    Description = "Root Warehouse - Österreich",
+                    HopType = "Warehouse",
+                    LocationCoordinates = new DataAccess.Entities.GeoCoordinate { Lat = 47.247829, Lon = 13.884382 },
+                    LocationName = "Root",
+                    ProcessingDelayMins = 186,
+                    Level = 0,
+                    NextHops = new List<DataAccess.Entities.WarehouseNextHops>()
+                });
+
+            var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingsProfileBlDal()); });
+
+            IWarehouseManagementLogic warehouseManagementLogic =
+                new WarehouseManagementLogic(mockWarehouseRepo.Object, mapperConfig.CreateMapper());
             Warehouse result = warehouseManagementLogic.GetWarehouse(code);
             Assert.NotNull(result);
         }
@@ -45,14 +102,30 @@ namespace UrbaniecZelenay.SKS.Package.BusinessLogic.Tests
         public void GetWarehouse_CodeNull_ExceptionThrown()
         {
             string code = null;
-            IWarehouseManagementLogic warehouseManagementLogic = new WarehouseManagementLogic();
+            Mock<IWarehouseRepository> mockWarehouseRepo = new Mock<IWarehouseRepository>();
+            mockWarehouseRepo.Setup(m => m.GetWarehouseByCode(It.IsAny<string>())).Returns(
+                new DataAccess.Entities.Warehouse
+                {
+                    Code = "AUTA01",
+                    Description = "Root Warehouse - Österreich",
+                    HopType = "Warehouse",
+                    LocationCoordinates = new DataAccess.Entities.GeoCoordinate { Lat = 47.247829, Lon = 13.884382 },
+                    LocationName = "Root",
+                    ProcessingDelayMins = 186,
+                    Level = 0,
+                    NextHops = new List<DataAccess.Entities.WarehouseNextHops>()
+                });
+
+            var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingsProfileBlDal()); });
+
+            IWarehouseManagementLogic warehouseManagementLogic =
+                new WarehouseManagementLogic(mockWarehouseRepo.Object, mapperConfig.CreateMapper());
             Assert.Throws<ArgumentNullException>(() => warehouseManagementLogic.GetWarehouse(code));
         }
 
         [Test]
         public void ImportWarehouses_ValidWarehouse_NoException()
         {
-            IWarehouseManagementLogic warehouseManagementLogic = new WarehouseManagementLogic();
             var validWarehouse = new Warehouse
             {
                 HopType = "Warehouse",
@@ -64,6 +137,11 @@ namespace UrbaniecZelenay.SKS.Package.BusinessLogic.Tests
                 Level = 0,
                 NextHops = new List<WarehouseNextHops>()
             };
+            Mock<IWarehouseRepository> mockWarehouseRepo = new Mock<IWarehouseRepository>();
+            var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingsProfileBlDal()); });
+
+            IWarehouseManagementLogic warehouseManagementLogic =
+                new WarehouseManagementLogic(mockWarehouseRepo.Object, mapperConfig.CreateMapper());
             warehouseManagementLogic.ImportWarehouses(validWarehouse);
             Assert.True(true);
         }
@@ -71,7 +149,11 @@ namespace UrbaniecZelenay.SKS.Package.BusinessLogic.Tests
         [Test]
         public void ImportWarehouses_InvalidWarehouseNextHopsNull_NoException()
         {
-            IWarehouseManagementLogic warehouseManagementLogic = new WarehouseManagementLogic();
+            Mock<IWarehouseRepository> mockWarehouseRepo = new Mock<IWarehouseRepository>();
+            var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingsProfileBlDal()); });
+
+            IWarehouseManagementLogic warehouseManagementLogic =
+                new WarehouseManagementLogic(mockWarehouseRepo.Object, mapperConfig.CreateMapper());
             var invalidWarehouse = new Warehouse
             {
                 HopType = "Warehouse",
@@ -86,10 +168,15 @@ namespace UrbaniecZelenay.SKS.Package.BusinessLogic.Tests
             Assert.Throws<ArgumentException>(() => warehouseManagementLogic.ImportWarehouses(invalidWarehouse));
             Assert.True(true);
         }
+
         [Test]
         public void ImportWarehouses_NullWarehouse_NoException()
         {
-            IWarehouseManagementLogic warehouseManagementLogic = new WarehouseManagementLogic();
+            Mock<IWarehouseRepository> mockWarehouseRepo = new Mock<IWarehouseRepository>();
+            var mapperConfig = new MapperConfiguration(mc => { mc.AddProfile(new MappingsProfileBlDal()); });
+
+            IWarehouseManagementLogic warehouseManagementLogic =
+                new WarehouseManagementLogic(mockWarehouseRepo.Object, mapperConfig.CreateMapper());
             Warehouse invalidWarehouse = null;
             Assert.Throws<ArgumentNullException>(() => warehouseManagementLogic.ImportWarehouses(invalidWarehouse));
             Assert.True(true);
