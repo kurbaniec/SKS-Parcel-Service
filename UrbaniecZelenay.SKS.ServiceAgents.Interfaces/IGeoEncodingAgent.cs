e396855f99c976d1cf696643061ad912d1d5cbe3
David Zelenay <david.zelenay@tietoevry.com>
2021-11-21 11:50:21 +0100

    Added Service Agent + Unit tests

---
+++ UrbaniecZelenay.SKS.ServiceAgents.Interfaces/IGeoEncodingAgent.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using BlGeoCoordinate = UrbaniecZelenay.SKS.Package.BusinessLogic.Entities.GeoCoordinate;
+
+namespace UrbaniecZelenay.SKS.ServiceAgents.Interfaces
+{
+    public interface IGeoEncodingAgent
+    {
+        BlGeoCoordinate EncodeAddress(string street, string postalCode, string city, string country);
+    }
+}
