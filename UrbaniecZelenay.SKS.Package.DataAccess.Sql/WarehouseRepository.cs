e66c980dfb1b2995a9806f71858f82931784d1d2
David Zelenay <david.zelenay@tietoevry.com>
2021-12-12 01:04:14 +0100

    changed truncate to delete

--- UrbaniecZelenay.SKS.Package.DataAccess.Sql/WarehouseRepository.cs
+++ UrbaniecZelenay.SKS.Package.DataAccess.Sql/WarehouseRepository.cs
@@ -37,12 +37,13 @@ namespace UrbaniecZelenay.SKS.Package.DataAccess.Sql
                 // context.Database.EnsureDeleted();
                 if (!IsUnitTest)
                 {
-                    context.Database.ExecuteSqlRaw(@"-- disable all constraints
-                EXEC sp_MSForEachTable ""ALTER TABLE ? NOCHECK CONSTRAINT all""
-                    -- delete data in all tables
-                EXEC sp_MSForEachTable ""DELETE FROM ?""
-                    -- enable all constraints
-                    exec sp_MSForEachTable ""ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all""");
+                    context.Database.ExecuteSqlRaw(@"DELETE FROM WarehouseNextHops;
+DELETE FROM PreviousHops;
+DELETE FROM HopArrivals;
+DELETE FROM Recipients;
+DELETE FROM Webhooks;
+DELETE FROM Hops;
+DELETE FROM Parcels;");
                 }
 
                 context.Database.EnsureCreated();
